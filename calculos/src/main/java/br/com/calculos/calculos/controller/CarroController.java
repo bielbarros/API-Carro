f368fac31d1545b435ed75e1fa11f9a618a10fd9
biel.barros <gabrielbarrosg11@gmail.com>
2025-04-15 13:27:13 -0300

    feat: Improving controller commands

--- calculos/src/main/java/br/com/calculos/calculos/controller/CarroController.java
+++ calculos/src/main/java/br/com/calculos/calculos/controller/CarroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/carro")
+@RequestMapping("/api/carro")
 public class CarroController {
 
     @Autowired
@@ -20,38 +20,55 @@ public class CarroController {
     public ResponseEntity<String> save(@RequestBody Carro carro){
 
         try {
-
             String mensagem = this.carroService.save(carro);
             return new ResponseEntity<String>(mensagem, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<String>("Deu algo errado ao salvar", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> update(@RequestBody Carro carro, @PathVariable Long id) {
+
+
+        try {
+            String mensagem = this.carroService.update(carro, id);
+            return new ResponseEntity<>(mensagem, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        try {
+            String mensagem = this.carroService.delete(id);
+            return new ResponseEntity<>(mensagem, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Carro>> findAll(){
+        try {
+            List<Carro> lista = this.carroService.findAll();
+            return new ResponseEntity<>(lista, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
    @GetMapping("/findById/{id}")
     public ResponseEntity<Carro> findById(@PathVariable Long id){
         try {
-
             Carro carro = this.carroService.findById(id);
             return new ResponseEntity<>(carro, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
-
-    @GetMapping("/lista")
-    public List<Carro>listarCarros(){
-        return carroService.listarCarros();
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarCarro(@PathVariable Long id) {
-        carroService.deletarCarros(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PutMapping("/{id}")
-    public Carro atualizarCarro(@PathVariable Long id, @RequestBody Carro carro) {
-        return carroService.atualizarCarro(id, carro);
-    }
-
 }
